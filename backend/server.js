86f39f1c455393a0f27e372ec8daecd2c88c0d60
Rohit Thakkar <rohitthakkar2021@gmail.com>
2026-03-08 19:50:24 +0530

    use  route controller

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,41 @@ require("dotenv").config();
 const cors = require("cors");
 
 const connectDB = require("./app/db/config/db");
+
+// ROUTES
+const authRoutes = require("./app/routes_controller/Auth")
 const residentRoutes = require("./app/routes_controller/Resident");
-const facilityBookingRoutes = require('./app/routes_controller/FacilityBooking')
-const complaintRoutes = require("./app/routes_controller/Complaint")
+const facilityBookingRoutes = require("./app/routes_controller/FacilityBooking");
+const complaintRoutes = require("./app/routes_controller/Complaint");
+
+// MIDDLEWARE
+const errorHandler = require("./app/middlewares/errorMiddleware");
+const authMiddleware = require("./app/middlewares/authMiddleware")
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+/* ---------------- DATABASE ---------------- */
+
 connectDB();
+
+/* ---------------- GLOBAL MIDDLEWARE ---------------- */
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Home page");
-});
+/* ---------------- ROUTES ---------------- */
 
-app.use("/api/resident", residentRoutes);
-app.use("/api/facility-booking", facilityBookingRoutes);
-app.use("/api/complaint", complaintRoutes);
+app.use("/api/auth", authRoutes );
+app.use("/api/resident",authMiddleware, residentRoutes);
+app.use("/api/facility-booking",authMiddleware, facilityBookingRoutes);
+app.use("/api/complaint",authMiddleware, complaintRoutes);
 
+/* ---------------- ERROR HANDLER ---------------- */
+
+app.use(errorHandler);
+
+/* ---------------- SERVER ---------------- */
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
