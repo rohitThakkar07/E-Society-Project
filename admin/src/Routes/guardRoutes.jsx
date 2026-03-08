9200bb24d00041745f92a15220cd6ac72407f60a
Rohit Thakkar <rohitthakkar2021@gmail.com>
2026-03-08 19:46:53 +0530

    create guard route

--- admin/src/Routes/guardRoutes.jsx
+++ admin/src/Routes/guardRoutes.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import Guards from "../components/pages/Guards";
+import Guards from "../components/pages/Guards/Guards";
+import AddGuard from '../components/pages/Guards/AddGuard';
 
 const guardRoutes = [
   {
     path: "guards",
     element: <Guards />,
+  },
+  {
+    path: "/guards/add",
+    element: <AddGuard />,
   },
 ];
 
