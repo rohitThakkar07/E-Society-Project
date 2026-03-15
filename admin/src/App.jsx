b3414b71cdd230039667cbbfe43f8524d7da43f6
Rohit Thakkar <rohitthakkar2021@gmail.com>
2026-03-15 10:55:45 +0530

    add toast

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Router from './Routes/index';
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 function App() {
   return (
-     <RouterProvider router={Router} />
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="colored"
+      />
+      <RouterProvider router={Router} />
+    </>
   );
 }
 
