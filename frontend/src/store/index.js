812d9e85f6398ab8b917a42ffd640a2efa459e38
Rohit Thakkar <rohitthakkar2021@gmail.com>
2026-03-26 14:20:00 +0530

    udpate store and auth contaxt

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,26 +1,33 @@
-// store/index.js
 import { configureStore } from "@reduxjs/toolkit";
-import residentReducer from "./slices/residentSlice";
-import authReducer from "./slices/authSlice";
-import guardReducer from "./slices/guardSlice";
-import visitorReducer from "./slices/visitorSlice";
-import complaintReducer from './slices/complaintSlice'
-import facilityReducer from './slices/facilitySlice'
-import bookingReducer from './slices/facilityBookingSlice'
-import maintenanceReducer from './slices/maintenanceSlice'
-import expenseReducer from "./slices/expenseSlice"; 
+import residentReducer        from "./slices/residentSlice";
+import authReducer            from "./slices/authSlice";
+import guardReducer           from "./slices/guardSlice";
+import visitorReducer         from "./slices/visitorSlice";
+import complaintReducer       from "./slices/complaintSlice";
+import facilityReducer        from "./slices/facilitySlice";
+import bookingReducer from "./slices/facilityBookingSlice"; 
+import maintenanceReducer     from "./slices/maintainenceSlice";
+import expenseReducer         from "./slices/expenseSlice";
+import noticeReducer  from "./slices/noticeSlice";
+import eventReducer   from "./slices/eventSlice";
+import flatReducer from './slices/flatSlice';
+import staffReducer from './slices/staffSlice';
 
 export const store = configureStore({
   reducer: {
-    resident: residentReducer,
-    auth: authReducer,
-    guard: guardReducer,
-    visitor: visitorReducer,
-    complaint: complaintReducer,
-    facility: facilityReducer, 
-    booking:bookingReducer,
-    maintenance: maintenanceReducer,
-    expense: expenseReducer, 
+    resident:        residentReducer,
+    auth:            authReducer,
+    guard:           guardReducer,
+    visitor:         visitorReducer,
+    complaint:       complaintReducer,
+    maintenance:     maintenanceReducer,
+    expense:         expenseReducer,
+    notice:          noticeReducer,
+    event:           eventReducer,
+    flat: flatReducer,
+    staff: staffReducer,
+    booking: bookingReducer, 
+    facility: facilityReducer,
   },
 });
 
