e576d1f3f976ac4e7f32126192f104e68b663f77
Rohit Thakkar <rohitthakkar2021@gmail.com>
2026-03-29 08:39:09 +0530

    updated falt modal

--- backend/app/db/models/flatModal.js
+++ backend/app/db/models/flatModal.js
@@ -25,46 +25,5 @@ const flatSchema = new mongoose.Schema({
   //parkingSlot: { type: String },
   monthlyMaintenance: { type: Number, default: 0 },
 }, { timestamps: true });
-
+flatSchema.index({ flatNumber: 1, block: 1 }, { unique: true });
 module.exports = mongoose.model("Flat", flatSchema);
-
-// const mongoose = require("mongoose");
-
-// const flatSchema = new mongoose.Schema(
-//   {
-//     wing: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//       uppercase: true
-//     },
-
-//     flatNumber: {
-//       type: String,
-//       required: true,
-//       trim: true
-//     },
-
-//     floorNumber: {
-//       type: Number,
-//       required: true
-//     },
-
-//     flatType: {
-//       type: String,
-//       required: true,
-//       enum: ["1BHK", "2BHK", "3BHK", "4BHK"]
-//     },
-
-//     status: {
-//       type: String,
-//       enum: ["Occupied", "Vacant"],
-//       default: "Vacant"
-//     }
-//   },
-//   { timestamps: true }
-// );
-
-// flatSchema.index({ wing: 1, flatNumber: 1 }, { unique: true });
-
-// module.exports = mongoose.model("Flat", flatSchema);
