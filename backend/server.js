a7e8dbd2f4a838f8af43383934ba05da00c7477c
Rohit Thakkar <rohitthakkar2021@gmail.com>
2026-03-30 23:07:19 +0530

    add attachment image in complaint modul and fix bug

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,9 @@ app.use(express.urlencoded({ extended: true }));
 // ✅ SERVE UPLOADED FILES
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+//upload image
+app.use("/uploads", express.static("uploads"));
+
 /* ============ ROUTES ============ */
 app.use("/api/auth", authRoutes);
 app.use("/api/resident", authMiddleware, residentRoutes);
